80d43a0a44f99f2c4cbe985a9896a40394a78597
buzzdao <34927799+buzzdao@users.noreply.github.com>
2018-01-04 15:42:43 +0800

    services

---
+++ roles/xmr-stak/scripts/xmrstak20.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+# /etc/init.d/xmrstak20
+# Carry out specific functions when asked to by the system
+cpumun=`cat /proc/cpuinfo| grep "processor"| wc -l`
+declare -i sum=$cpumun
+case "$1" in
+  start)
+    echo "Starting miner script"
+    export LD_LIBRARY_PATH=/root/xmr-stak/
+    sudo tsocks /root/xmr-stak/build/bin/xmr-stak -c /root/xmr-stak/build/bin/config.txt --cpu /root/xmr-stak/build/bin/cpu$sum.txt &
+    ;;
+  stop)
+    echo "Stopping miner script"
+    sudo killall xmr-stak
+    ;;
+  *)
+    echo "Usage: /etc/init.d/xmrstak20 {start|stop}"
+    exit 1
+    ;;
+esac
+exit 0
---
+++ roles/xmr-stak/scripts/xmrstak22.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+# /etc/init.d/xmrstak22
+# Carry out specific functions when asked to by the system
+rm -f /home/administrator/cpu.txt
+cpumun=`cat /proc/cpuinfo| grep "processor"| wc -l`
+declare -i sum=$cpumun
+case "$1" in
+  start)
+    echo "Starting miner script"
+    export LD_LIBRARY_PATH=/root/xmr-stak22/
+    sudo tsocks /root/xmr-stak22/build/bin/xmr-stak -c /root/xmr-stak22/build/bin/config.txt &
+    ;;
+  stop)
+    echo "Stopping miner script"
+    sudo killall xmr-stak
+    ;;
+  *)
+    echo "Usage: /etc/init.d/xmrstak22 {start|stop}"
+    exit 1
+    ;;
+esac
+exit 0
