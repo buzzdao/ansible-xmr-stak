3b153b08495ee388fec67bc21184ef85bbc4bc19
buzzdao <34927799+buzzdao@users.noreply.github.com>
2018-10-16 09:00:36 +0800

    1

--- roles/xmr-stak/scripts/xmrstak250.sh
+++ roles/xmr-stak/scripts/xmrstak250.sh
@@ -2,13 +2,13 @@
 # /etc/init.d/xmrstak250
 # Carry out specific functions when asked to by the system
 #rm -f /home/administrator/cpu.txt
-sudo sysctl -w vm.nr_hugepages=32
+#sudo sysctl -w vm.nr_hugepages=32
 case "$1" in
   start)
     echo "Starting miner script"
     export LD_LIBRARY_PATH=/root/xmr-stak250/
 #    sudo tsocks xmr-stak
-    sudo tsocks /root/xmr-stak250/build/bin/xmr-stak -c /root/xmr-stak250/build/bin/config.txt -C /root/xmr-stak250/build/bin/pools.txt &
+    sudo tsocks /root/xmr-stak250/build/bin/xmr-stak -c /root/xmr-stak250/build/bin/config.txt -C /root/xmr-stak250/build/bin/pools.txt
     ;;
   stop)
     echo "Stopping miner script"
