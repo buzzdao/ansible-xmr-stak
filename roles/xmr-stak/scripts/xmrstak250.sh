0e5fe4452dc6e031cdb45fd327ef8cad79784ab5
buzzdao <34927799+buzzdao@users.noreply.github.com>
2018-10-18 12:14:10 +0800

    Update roles/xmr-stak/scripts/xmrstak250.sh

--- roles/xmr-stak/scripts/xmrstak250.sh
+++ roles/xmr-stak/scripts/xmrstak250.sh
@@ -8,7 +8,7 @@ case "$1" in
     echo "Starting miner script"
     export LD_LIBRARY_PATH=/root/xmr-stak250/
 #    sudo tsocks xmr-stak
-    sudo tsocks /root/xmr-stak250/build/bin/xmr-stak -c /root/xmr-stak250/build/bin/config.txt -C /root/xmr-stak250/build/bin/pools.txt
+    sudo tsocks /root/xmr-stak250/build/bin/xmr-stak -c /root/xmr-stak250/build/bin/config.txt -C /root/xmr-stak250/build/bin/pools.txt &
     ;;
   stop)
     echo "Stopping miner script"
